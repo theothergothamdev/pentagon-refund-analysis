294d27f9462e262fbfd26053f4da2af62291efa8
theothergothamdev <gothamdev@outlook.com>
2022-05-19 08:50:02 +0200

    chore: Initial commit

---
+++ log-parser/index.js
@@ -0,0 +1,94 @@
+const fs = require('fs');
+
+const parseWalletLine = line => line.replace("['", '').replace("']", '');
+const peak = arr => arr[0];
+const isWalletLine = line => line.startsWith("['0x");
+
+function findDuplicates(entries) {
+  const occurances = {};
+
+  for (const entry of entries) {
+    if (!occurances[entry.walletAddress]) {
+      occurances[entry.walletAddress] = 0;
+    }
+
+    occurances[entry.walletAddress] = occurances[entry.walletAddress] + 1;
+  }
+  const duplicates = [];
+
+  for (const [walletAddress, count] of Object.entries(occurances)) {
+    if (count > 1) {
+      duplicates.push(walletAddress);
+    }
+  }
+  return duplicates;
+}
+
+function findUniqueWalletCount(entries) {
+  const uniqueWallets = new Set(entries.map(entry => entry.walletAddress));
+  return uniqueWallets.size;
+}
+
+function parseWalletBlock(walletAddress, walletBlock) {
+  console.warn('Processing Block', walletBlock);
+
+  const walletSummary = {};
+  let refundTotal = 0;
+  let txns = [];
+  while (walletBlock.length > 0) {
+    const line = walletBlock.shift();
+    if (line.startsWith('refunded')) {
+      const parts = line.split(' ');
+      refundTotal += parseInt(parts[1]);
+      txns.push(parts[8]);
+    }
+  }
+
+  walletSummary.walletAddress = walletAddress;
+  walletSummary.txns = txns;
+  walletSummary.refundTotal = refundTotal;
+  return walletSummary;
+}
+
+function parseLogs(logs) {
+  const summary = {
+    refunded: [],
+    notRefunded: [],
+  };
+
+  while (logs.length > 0) {
+    const line = logs.shift();
+    if (isWalletLine(line)) {
+      const walletAddress = parseWalletLine(line);
+      const block = [line];
+
+      while (true) {
+        if (logs.length === 0 || isWalletLine(peak(logs))) {
+          const walletSummary = parseWalletBlock(walletAddress, block);
+          if (walletSummary.refundTotal > 0) {
+            summary.refunded.push(walletSummary);
+          } else {
+            summary.notRefunded.push(walletSummary);
+          }
+          break;
+        }
+
+        block.push(logs.shift());
+      }
+    }
+  }
+
+  summary.totalWalletsNotRefunded = findUniqueWalletCount(summary.notRefunded);
+  summary.totalWalletsRefunded = findUniqueWalletCount(summary.refunded);
+  summary.totalWallets = summary.totalWalletsNotRefunded + summary.totalWalletsRefunded;
+  summary.refunded = summary.refunded.sort((a, b) => b.refundTotal - a.refundTotal);
+  summary.duplicates = findDuplicates(summary.refunded);
+  return summary;
+}
+
+const logFile = fs.readFileSync('logs.txt', { encoding: 'utf-8' });
+const logs = logFile.split('\n');
+const summary = parseLogs(logs);
+console.log(summary);
+
+fs.writeFileSync('summary.json', JSON.stringify(summary, null, 2));
---
+++ website/src/App.tsx
@@ -0,0 +1,71 @@
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import InputBase from '@mui/material/InputBase';
+import IconButton from '@mui/material/IconButton';
+import SearchIcon from '@mui/icons-material/Search';
+
+import Alert from '@mui/material/Alert';
+
+import data from './data.json';
+import { useState } from 'react';
+
+function App() {
+  const [textFilter, setTextFilter] = useState('');
+
+  const filteredData = textFilter
+    ? data.refunded.filter(item => item.walletAddress.indexOf(textFilter))
+    : [...data.refunded];
+
+  return (
+    <Container sx={{ p: 5 }}>
+      <Alert severity="info">
+        <div>Total wallets: {data.totalWallets}</div>
+        <div>Total wallets refunded: {data.totalWalletsRefunded}</div>
+        <div>Total wallets not refunded: {data.totalWalletsNotRefunded}</div>
+      </Alert>
+      <Paper component="form" sx={{ p: '2px 4px', my: 2, display: 'flex', alignItems: 'center' }}>
+        <IconButton type="submit" sx={{ p: '10px' }} aria-label="search">
+          <SearchIcon />
+        </IconButton>
+        <InputBase
+          sx={{ ml: 1, flex: 1 }}
+          placeholder="Filter Results"
+          inputProps={{ 'aria-label': 'filter results' }}
+          value={textFilter}
+          onChange={event => {
+            setTextFilter(event.target.value);
+          }}
+        />
+      </Paper>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Wallet Address</TableCell>
+              <TableCell align="right">Refund Total (MATIC)</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {filteredData.map((row, index) => (
+              <TableRow key={index} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                <TableCell component="th" scope="row">
+                  <a href={`https://polygonscan.com/address/${row.walletAddress}`}>{row.walletAddress}</a>
+                </TableCell>
+                <TableCell align="right">{row.refundTotal}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Container>
+  );
+}
+
+export default App;
